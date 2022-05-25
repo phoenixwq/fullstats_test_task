be9252e0f3475fe038c66b8c1e0d210d45464710
Timur <phoenixwq01@gmail.com>
2022-05-26 00:24:22 +0300

    init project

---
+++ src/posts/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+UserModel = get_user_model()
+
+
+class Post(models.Model):
+    article = models.PositiveIntegerField(db_index=True)
+    title = models.CharField(max_length=128, db_index=True)
+    overview = models.CharField(max_length=256)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField()
+    author = models.ForeignKey(
+        UserModel,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        null=True
+    )
+    users_marks = models.ManyToManyField(
+        UserModel,
+        through="Mark",
+        related_name="posts_marks"
+    )
+    users_visits = models.ManyToManyField(
+        UserModel,
+        through="Visit",
+        related_name="posts_visits"
+    )
+    users_favorites = models.ManyToManyField(
+        UserModel,
+        through="Favorite",
+        related_name="favorites_posts"
+    )
+
+    def get_visits_count(self):
+        return self.visit_set.count()
+
+    def get_likes_count(self):
+        return self.mark_set.filter(value=1).count()
+
+    def get_dislikes_count(self):
+        return self.mark_set.filter(value=-1).count()
+
+
+class UserPostBase(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        UserModel,
+        editable=False,
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(editable=False, auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        unique_together = ["post", "user"]
+
+
+class Mark(UserPostBase):
+    value = models.IntegerField(
+        validators=[MinValueValidator(-1),
+                    MaxValueValidator(1)],
+        default=0
+    )
+
+
+class Favorite(UserPostBase):
+    pass
+
+
+class Visit(UserPostBase):
+    user = models.ForeignKey(
+        UserModel,
+        null=True,
+        editable=False,
+        on_delete=models.SET_NULL
+    )
+    ip = models.CharField(
+        max_length=40,
+        editable=False
+    )
+    session = models.CharField(
+        max_length=40,
+        editable=False
+    )
+    user_agent = models.CharField(
+        max_length=255,
+        editable=False
+    )
+
+    class Meta:
+        pass
